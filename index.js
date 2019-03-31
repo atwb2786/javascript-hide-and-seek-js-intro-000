e2777d08781114ac12c9b0927b2eaaa65435ac76
punctual-sailor-2214 <bchett286@yahoo.com>
2019-03-31 17:59:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 
+function getFirstSelector(selector) {
+    return document.querySelector(selector);
+}
+
+function nestedTarget() {
+    return document.getElementById("nested").querySelector(".target");
+}
+
+function deepestChild() {
+    var list = document.getElementById("grand-node").querySelectorAll("div");
+    var test;
+    for (let i = 0; i < list.length - 1; i++) {
+      test = list[i].querySelector("div")
+    }
+    return test;
+}
+
+function increaseRankBy(n) {
+    
+}
